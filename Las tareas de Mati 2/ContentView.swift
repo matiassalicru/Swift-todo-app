6a22c1fbd258ab899811b1243122c81c70024581
Matias Salicru <40377128+matiassalicru@users.noreply.github.com>
2026-03-09 11:17:00 -0300

    Initial Commit

---
+++ Las tareas de Mati 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Las tareas de Mati 2
+//
+//  Created by Matias Salicru on 09/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Las tareas de Mati 2/Las_tareas_de_Mati_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Las_tareas_de_Mati_2App.swift
+//  Las tareas de Mati 2
+//
+//  Created by Matias Salicru on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Las_tareas_de_Mati_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
